e615d746b007f3ea2e17ef4bb908d38ea53839d6
farukfilikci <135740843+farukfilikci@users.noreply.github.com>
2023-07-14 23:09:57 +0300

    Update project.py

--- project.py
+++ project.py
@@ -1,8 +1,1 @@
-# projedeki ilk degisiklik
-print("Projede birinci degisiklik yapildi")
 
-#ikinci degisiklik
-print("Projede ikinci degisiklik yapildi")
-
-#ucuncu degisiklik
-print("ucuncu degisiklik yapildi")
