66b29b307be289a6fca0a4255464c713f2bb5c19
farukfilikci <135740843+farukfilikci@users.noreply.github.com>
2023-07-14 23:10:23 +0300

    project.py

--- project.py
+++ project.py
@@ -1,1 +1,25 @@
+import hashlib
 
+def hash_password(password):
+    hashed_password = hashlib.sha256(password.encode()).hexdigest()
+    return hashed_password
+
+def check_password_complexity(password):
+    length = len(password)
+    has_upper = any(char.isupper() for char in password)
+    has_lower = any(char.islower() for char in password)
+    has_digit = any(char.isdigit() for char in password)
+    has_special = any(not char.isalnum() for char in password)
+
+    if length >= 8 and has_upper and has_lower and has_digit and has_special:
+        print("password safe")
+    else:
+        print("password not safe")
+
+def main():
+    password = input("please enter your password: ")
+    hashed_password = hash_password(password)
+    check_password_complexity(password)
+
+if __name__ == "__main__":
+    main()
